76235214f341a821f678826e7da20787d9e8d06a
Cole Maxwell <cole.h.maxwell@gmail.com>
2020-12-22 00:17:19 -0500

    updated test fixture

--- test/src/edu/umb/cs680/hw09/fs/ApfsFileSystemTest.java
+++ test/src/edu/umb/cs680/hw09/fs/ApfsFileSystemTest.java
@@ -1,14 +1,20 @@
 package edu.umb.cs680.hw09.fs;
+import edu.umb.cs680.hw09.apfs.*;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ApfsFileSystemTest {
-	
+
 	@Test
-	public void verifySomething()
-	{
-
+	public void verifyRootDirectoryNameEquality() {
+		APFS apfs = new APFS("new apfs", 1024);
+		apfs.initFileSystem("new apfs", 1024);
+		ApfsDirectory root = new ApfsDirectory(null, "root", 0, LocalDateTime.now(), null); 
+		apfs.appendRootDir(root);
+		Object expected = "root";
+		Object actual = root.getName();
+		assertEquals(expected, actual);
 	}
-
 }
